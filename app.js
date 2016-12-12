ed0e14345922a957c4a7b0ad286b17504e2d279b
ebull53-personal <ebull53@gmail.com>
2016-12-12 13:10:06 -0600

    6

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.post('/collect', function(req, res){
 	//Structure Data
 	var data = {
 		v: 		1,
-		tid: 	"UA-88852239-2",
+		tid: 	env_var.ga_key,
 		cid: 	user.id,
 		ds:  	"slack", //data source
 		cs: 	"slack", // campaign source
